444dac76b273068c6e62c3defb834a3f69c50939
sudhe <sudhe@LAPTOP-H29EQDPO>
2021-06-06 00:09:43 +0530

    first commit

---
+++ MFFTest/src/mfftest/RawTest.java
@@ -0,0 +1,15 @@
+package mfftest;
+
+public class RawTest {
+
+	public static void main(String[] args) {
+		
+		String a = "DEV";
+		
+		String b ="OPS";
+		
+		System.err.println(a+b);
+		
+	}
+
+}
